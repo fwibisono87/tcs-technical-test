baf59731b7d0132ef453b853f124a8cfbd1b665b
Francis Wibisono <francis@pusilkom.com>
2026-01-22 14:37:24 +0700

    feat: init repo, tailwind, fonts, icons

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center bg-gray-800">
+    <h1 class="h1">setup ok</h1>
+  </div>
+</template>
